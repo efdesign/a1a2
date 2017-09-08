26232ff0bb59e975c0e5ccec81e787e449a3e904
enrico <iwannapose@gmail.com>
2017-09-08 22:39:15 +0200

    at this time we have a global dependency on jquery and no module loader, tatic scripts and so on, in the next step we'll introduce systemjs

--- src/test.directive.js
+++ src/test.directive.js
@@ -5,6 +5,10 @@ angular.module('test').directive('fooBar', function () {
         restrict: 'E',
         controller: 'test.controller',
         controllerAs: '$ctrl',
+        link:function(elm,attr){
+            // fake some global dependency here...for later
+            console.log(jQuery.fn.jquery);
+        }
         
     }
 });
